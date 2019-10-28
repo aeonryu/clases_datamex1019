601983d81cf4449e7429fb8bde8a294170724b67
YonatanRA <yeho@mail.com>
2019-10-28 08:11:41 -0600

    w2_d1

--- queries(w2_d1).sql
+++ queries(w2_d1).sql
@@ -31,3 +31,36 @@ SELECT
     ORDER BY numautores DESC;
 
 
+# Alias
+SELECT q.title FROM publis.titles as  q;
+
+# Mutliple JOINs
+SELECT titleauthor.au_id, titles.title FROM titleauthor 
+	LEFT JOIN titles ON titleauthor.title_id = titles.title_id
+	RIGHT JOIN titles ON titleauthor.title_id = titles.title_id;
+    
+# Concatenate string columns
+SELECT CONCAT(au_lname, " ", au_fname) AS FullName FROM authors;
+
+# Multiple joins grouped by title with concatenated authors
+SELECT title, GROUP_CONCAT(CONCAT(au_lname, " ", au_fname) SEPARATOR ", "), count(*)
+FROM titles as t 
+	INNER JOIN titleauthor as ta ON t.title_id = ta.title_id
+    INNER JOIN authors as au ON ta.au_id = au.au_id
+    GROUP BY t.title;
+    
+# Union
+SELECT  au_id, city as m FROM authors WHERE state="IN"
+UNION
+SELECT au_id, royaltyper as m FROM  titleauthor;
+
+
+# Subquery
+SELECT stor_id, m.title, m.title_id, payterms  FROM sales 
+INNER JOIN titles as m ON sales.title_id = m.title_id
+WHERE payterms != "Net 30" 
+	AND  m.title_id IN (
+		SELECT titles.title_id FROM sales 
+		INNER JOIN titles ON sales.title_id = titles.title_id
+		WHERE payterms = "Net 30"
+	);
