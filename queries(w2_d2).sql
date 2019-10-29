606a21278d70f685d7b725d9f04dff48b1f92ce0
YonatanRA <yeho@mail.com>
2019-10-29 09:07:01 -0600

    w2_d2

--- queries(w2_d2).sql
+++ queries(w2_d2).sql
@@ -1,1 +1,37 @@
 
+
+select stores.stor_name as store,
+count(distinct(ord_num)) as orders,
+count(title_id) as items, sum(qty) as qty
+from publications.sales sales 
+inner join publications.stores stores
+on stores.stor_id=sales.stor_id
+group by store;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
